d0cd639af952738862e616ef468bf4a586ce9473
johnn3volta <johnn3volta@gmail.com>
2018-12-18 11:30:57 +0300

    add form validation

--- source/js/form.js
+++ source/js/form.js
@@ -1,26 +1,48 @@
-(function () {
-    var me = {},
-        form = document.querySelector('.form-container'),
-        closeButton = null;
+(function(){
+  var me = {},
+    form = document.querySelector('.form-container'),
+    closeButton = null;
 
-    function onClose(e){
-        e.preventDefault();
+  function onClose(e)
+  {
+    e.preventDefault();
 
-        me.close();
-        closeButton.removeEventListener('click', onClose)
+    me.close();
+    closeButton.removeEventListener('click', onClose);
+  }
+
+  me.open = function(){
+    form.classList.remove('is-hidden');
+
+    closeButton = document.querySelector('.form__close-button');
+    closeButton.addEventListener('click', onClose);
+  };
+
+  me.close = function(){
+    form.classList.add('is-hidden');
+  };
+
+  me.isValid = function(){
+
+    if (!me.isAllCompleted(document.querySelectorAll('[data-valid="required"]'))){
+      console.log('Заполните все необходимые поля плиз');
+      return false;
     }
+    return true;
+  };
 
-    me.open = function () {
-        form.classList.remove('is-hidden');
+  me.isAllCompleted = function(data){
+    var result = true;
 
-        closeButton = document.querySelector('.form__close-button');
-        closeButton.addEventListener('click',onClose)
-    };
+    for (var i = 0; i < data.length; i++){
+      if (!ITVDN.validation.isNotEmpty(data[i].value)){
+        result = false;
+        break;
+      }
+    }
+    return result;
+  };
 
-    me.close = function () {
-        form.classList.add('is-hidden')
-    };
-
-    window.form = me;
+  ITVDN.form = me;
 
 }());
--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,26 @@
-(function () {
+(function(){
 
-    function $(selector, el) {
-        if (!el) {
-            el = document;
-        }
+  // Add vent listener for open from button
 
-        return el.querySelector(selector)
-    }
+  var openFormButton = document.querySelector('.arrow-down');
+  var form = document.querySelector('.form');
 
-    'use strict';
-    // Add vent listener for open from button
+  if (openFormButton){
+    openFormButton.addEventListener('click', function(e){
+      e.preventDefault();
+      ITVDN.form.open();
+    });
+  }
 
-    var openFormButton = document.querySelector('.arrow-down');
-
-    if (openFormButton) {
-        openFormButton.addEventListener('click', function (e) {
-            e.preventDefault();
-            form.open();
-        });
-    }
+  if (form){
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+      if (ITVDN.form.isValid()){
+        console.log('All good');
+      }else{
+        console.log('Warning');
+      }
+    });
+  }
 
 }());
